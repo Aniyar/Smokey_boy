c8bb76e3fe7f452701df187cbaf3c51ac646bc24
LillyLees <climbgeekv7@gmail.com>
2021-08-13 11:45:05 +0100

    Implement basic evolution

--- AI/ai.py
+++ AI/ai.py
@@ -4,33 +4,91 @@ from .simulation.interventions import *
 import numpy as np
 from copy import deepcopy
 
+
 def fitness(X):
     return -np.sum(X[7, :, :])
 
+def frick(a, b):
+    return a + b
+
 if __name__ == '__main__':
     N = 5
+    generations = 2
     simulation = SimulationManager()
     # Create initial state
     initial_state = init_del_loma_smol()
     initial_state[7, 150, 150] = 1
     available_resources = {
-        'bulldozer': 3
+        'bulldozer': 30
     }
     # Come up with intervention plans
-    intervention_plans = [[
+    population = [[
         BullDozedFireLine(
-            (150 + np.random.randint(30) - 15, 150 + np.random.randint(30) - 15),
-            (150 + np.random.randint(30) - 15, 150 + np.random.randint(30) - 15)
+            (150 + np.random.randint(30) - 15, 
+            150 + np.random.randint(30) - 15),
+            (150 + np.random.randint(30) - 15, 
+            150 + np.random.randint(30) - 15)
         ),
     ] for i in range(N)]
+    # THE CIRCLE OF LIFE
+    for generation in range(generations):
+        # Test intervention plans
+        final_states = [
+            simulation.run_simulation(
+                deepcopy(initial_state), 
+                deepcopy(available_resources),
+                deepcopy(plan),
+                steps=30,
+                visualise=False
+            )
+            for plan in population
+        ]
+        # Evaluate the final states
+        final_scores = [(i, fitness(state))
+                        for i, state in enumerate(final_states)]
+        # Generate rankings
+        final_score_rankings = sorted(
+            final_scores, 
+            key=lambda x: x[1], 
+            reverse=True
+        )
+        # Cull the bottom 50% and discard the fitnesses
+        survivors = final_score_rankings[:int(len(final_score_rankings) / 2)]
+        survivors = [population[i] for i, fitness in survivors]
+        # Make the fittest breed
+        new_population = deepcopy(survivors)
+        for i in range(N - len(survivors)):
+            a, b = np.random.choice(len(survivors), 2)
+            new_population.append(frick(survivors[a], survivors[b]))
+        population = new_population
+
     # Test intervention plans
     final_states = [
-        simulation.run_simulation(deepcopy(initial_state), deepcopy(available_resources), deepcopy(plan), steps=30, visualise=False)
-        for plan in intervention_plans
+        simulation.run_simulation(
+            deepcopy(initial_state), 
+            deepcopy(available_resources),
+            deepcopy(plan),
+            steps=30,
+            visualise=False
+        )
+        for plan in population
     ]
     # Evaluate the final states
-    final_scores = [fitness(state) for state in final_states]
-    best, worst = np.argmax(final_scores), np.argmin(final_scores)
-    print(best, worst)
-    simulation.run_simulation(deepcopy(initial_state), deepcopy(available_resources), deepcopy(intervention_plans[best]), steps=30, visualise=True)
-    simulation.run_simulation(deepcopy(initial_state), deepcopy(available_resources), deepcopy(intervention_plans[worst]), steps=30, visualise=True)
+    final_scores = [(i, fitness(state))
+                    for i, state in enumerate(final_states)]
+    # Generate rankings
+    final_score_rankings = sorted(
+        final_scores, 
+        key=lambda x: x[1], 
+        reverse=True
+    )
+    # Get the best one
+    best_plan = final_score_rankings[0][0]
+
+    simulation.run_simulation(
+        deepcopy(initial_state),
+        deepcopy(available_resources), 
+        deepcopy(population[best_plan]),
+        steps=30,
+        visualise=True
+    )
